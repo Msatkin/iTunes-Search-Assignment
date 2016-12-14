8df382743a6751011f6f47cd91cdea57dfb99d5c
Msatkin <mscottaa@gmail.com>
2016-12-14 09:10:54 -0600

    working with more css needed

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
+/* jshint -W041 */
 function mainSearch() {
-    var searchbar = document.getElementById('searchBox');
-    var searchDiv = document.getElementById('searchForm');
+    var searchbar = $('#searchBox');
+    var searchDiv = $('#searchForm');
+    var searchDivNav = $('#searchFormNav');
+    if (searchbar[0].value == "") {
+        return;
+    }
     $(searchDiv).hide();
+    $(searchDivNav).show();
+    var results = GetSearchResults(searchbar[0].value);
+}
+
+function GetSearchResults(searchCriteria) {
+    $.ajax({
+        url: 'http://itunes.apple.com/search?term=' + searchCriteria,
+        dataType: 'jsonp',
+        async: true,
+        success: function(data) {
+            DisplaySearchResults(data.results);
+        }
+    });
+}
+
+function DisplaySearchResults(results) {
+    console.log(results);
+
+    var resultDisplay = $('#result');
+    var resultList = $('#results-list');
+    for (var result in results) {
+        console.log("result", results[result]);
+        var newResult = resultDisplay.clone();
+        var title = results[result].trackName;
+        var artist = results[result].artistName
+        newResult.attr("id", "result" + result);
+        newResult.attr("hidden", false);
+        newResult.find('.albumArt').attr("src", results[result].artworkUrl100);
+        newResult.find('.songTitle').replaceWith("<p class=\"songTitle\">" + title + "</p>");
+        newResult.find('.songArtist').replaceWith("<p class=\"songArtist\">" + artist + "</p>");
+        newResult.find('.audioPlayer').attr("src", results[result].previewUrl);
+        resultList.append(newResult);
+    }
+}
+
+function enterPress(keyPressed) {
+    if (keyPressed == 13) {
+        mainSearch();
+    }
 }
