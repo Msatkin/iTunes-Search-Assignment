994dfa34e743ca8643328ab1ea4594af89c5c916
Msatkin <mscottaa@gmail.com>
2016-12-15 10:35:17 -0600

    nav bar search working

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 /* jshint -W041 */
-var defaultResultList;
-$(document).ready(function() {
-  defaultResultList = $('#results-list');
-});
+var songBlocks = [];
+
 function mainSearch() {
-  console.log('searching...');
     var searchBar = $('#searchBox')[0];
     var searchDiv = $('#searchForm')[0];
     var searchDivNav = $('#searchFormNav')[0];
     var searchBarNav = $('#searchBoxNav')[0];
     var results = null;
     var criteria;
-    console.log("display", searchDiv.style.display);
     if (searchDiv.style.display == 'none') {
       if (searchBarNav.value == "") {
           return;
       }
       criteria = searchBarNav.value;
-      console.log('navsearch', criteria);
+      searchBarNav.value = "";
     }
     else {
       if (searchBar.value == "") {
@@ -25,7 +21,9 @@ function mainSearch() {
       }
       criteria = searchBar.value;
       $(searchDiv).hide();
-      $(searchDivNav).show();
+      //$(searchDivNav).show();
+      searchDivNav.style.display = 'inline';
+      $('#paginationNav').show();
     }
     results = GetSearchResults(criteria);
 }
@@ -36,32 +34,47 @@ function GetSearchResults(searchCriteria) {
         dataType: 'jsonp',
         async: true,
         success: function(data) {
-            DisplaySearchResults(data.results);
+            FormatSearchResults(data.results);
+            CreatePagination();
+            displayPage(0);
         }
     });
 }
 
-function DisplaySearchResults(results) {
-    console.log(results);
-    var resultList = $('#results-list');
-    var resultDisplay = $('#result');
-    resultList.empty();
-    resultList.append(resultDisplay);
+function FormatSearchResults(results) {
+    songBlocks = [];
     for (var result in results) {
       var title = results[result].trackName;
       var artist = results[result].artistName;
       var newResult = '<li id="result' + result + '" class="results"><div><img class="albumArt img-responsive" src="' + results[result].artworkUrl100.replace('100x100','2180x2180') +'"/><div class="artCover"><p class="songTitle">' + title + '</p><p class="songArtist">' + artist + '</p><div class="audioControls"><audio controls class="audioPlayer" src="' + results[result].previewUrl + '"/></div></div></div></li>';
-      resultList.append(newResult);
+      songBlocks.push(newResult);
     }
 }
 
-function onPressNav(keyPressed) {
-    if (keyPressed == 13) {
-        mainSearch();
-    }
+function CreatePagination() {
+  var pagesNeeded = songBlocks.length/20;
+  var paginationList = $('.pagination')[0];
+  $(paginationList).empty();
+
+  for (i = 0; i < pagesNeeded; i++) {
+    var newPage = '<li><span onclick="displayPage(' + i + ')">' + (i+1) + '</span></li>';
+    $(newPage).appendTo(paginationList);
+  }
 }
-function onPressMain(keyPressed, button) {
+
+function displayPage(pageNumber) {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  var displayList = $('#results-list')[0];
+  $(displayList).empty();
+  for (i = 0; i < 20; i ++) {
+    if (i + (20 * pageNumber) < songBlocks.length) {
+      $(songBlocks[i + (20 * pageNumber)]).appendTo(displayList);
+    }
+  }
+}
+
+function onPress(keyPressed) {
     if (keyPressed == 13) {
-      button.click();
+      mainSearch();
     }
 }
